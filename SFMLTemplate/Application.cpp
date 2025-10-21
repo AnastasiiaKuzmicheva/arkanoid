215f8fd9f70e9b8481b035a3e18222addb2b0ca3
unknown <_email@example.com>
2025-10-21 10:23:15 +0400

    Initial commit

---
+++ SFMLTemplate/Application.cpp
@@ -0,0 +1,71 @@
+#include <cstdlib>
+#include "Application.h"
+
+namespace ArkanoidGame
+{
+	Application& Application::Instance()
+	{
+		static Application instance;
+		return instance;
+	}
+
+	Application::Application() :
+		window(sf::VideoMode(ArkanoidGame::SCREEN_WIDTH, ArkanoidGame::SCREEN_HEIGHT), "ArkanoidGame")
+	{
+		//init random number generator
+		unsigned int seed = (unsigned int)time(nullptr); //get current time as seed. Yoy can also use any number to fix randomization
+		srand(seed);
+
+		InitGame(game);
+	}
+
+	Application::~Application()
+	{
+		ShutdownGame(game);
+	}
+
+	void Application::Run()
+	{
+		//init game clock
+		sf::Clock gameClock;
+
+		//Game loop
+		while (window.isOpen())
+		{
+			float startTime = gameClock.getElapsedTime().asSeconds();
+
+			HandleWindowEvents(game, window);
+
+			if (!window.isOpen())
+			{
+				break;
+			}
+
+			if (UpdateGame(game, TIME_PER_FRAME))
+			{
+				//draw everithing here
+				//clear the window first
+				window.clear();
+
+				DrawGame(game, window);
+
+				//end the current frame, display window contents on screen
+				window.display();
+			}
+			else
+			{
+				window.close();
+			}
+
+			float endTime = gameClock.getElapsedTime().asSeconds();
+			float deltaTime = endTime - startTime;
+			if (deltaTime < TIME_PER_FRAME)
+			{
+				//reduce framerate to not spam CPU and GPU
+				sf::sleep(sf::seconds(TIME_PER_FRAME - deltaTime));
+			}
+		}
+	}
+
+
+}
---
+++ SFMLTemplate/GameStateMainMenu.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "SFML/Graphics.hpp"
+#include "SFML/Audio.hpp"
+#include "Menu.h"
+
+namespace ArkanoidGame
+{
+	struct Game;
+
+	struct GameStateMainMenuData
+	{
+		sf::Font font;
+
+		MenuItem startGameItem;
+
+		MenuItem difficultyItem;
+		MenuItem difficultyEasyItem;
+		MenuItem difficultyNormalItem;
+		MenuItem difficultyHardItem;
+		MenuItem difficultyInsaneItem;
+		MenuItem difficultyImpossibleItem;
+
+		MenuItem optionsItem;
+		MenuItem optionsSoundItem;
+		MenuItem optionsMusicItem;
+
+		MenuItem recordsItem;
+		MenuItem exitGameItem;
+		MenuItem yesItem;
+		MenuItem noItem;
+		Menu menu;
+
+		sf::SoundBuffer soundBtnHoverBuffer;
+		sf::Sound soundBtnHover;
+	};
+
+	void InitGameStateMainMenu(GameStateMainMenuData& data);
+	void ShutdownGameStateMainMenu(GameStateMainMenuData& data);
+	void HandleGameStateMainMenuWindowEvent(GameStateMainMenuData& data, const sf::Event& event);
+	void UpdateGameStateMainMenu(GameStateMainMenuData& data, float timeDelta);
+	void DrawGameStateMainMenu(GameStateMainMenuData& data, sf::RenderWindow& window);
+}
+
